e68ef73010430b0992db13348d53ba29f97b8e6d
Jarosław Abram <Jaroslaw.Abram@ttpsc.pl>
2020-01-07 12:29:36 +0100

    init

---
+++ src/com/sample/model/Kweet.kt
@@ -0,0 +1,6 @@
+package com.sample.model
+
+import org.joda.time.*
+import java.io.*
+
+data class Kweet(val id: Int, val userId: String, val text: String, val date: DateTime, val replyTo: Int?) : Serializable
